ca0114808717d4b0a7e91fc71669c2d7ee3bb104
D3STY <nitram88@gmx.net>
2021-11-15 07:49:58 +0100

    Add dot_aliases.zsh

---
+++ dot_aliases.zsh
@@ -0,0 +1,9 @@
+mnt() { mount | awk -F' ' "{ printf \"%s\t%s\n\",\$1,\$3; }" | column -t | egrep ^/dev/ | sort }
+rm_rf() { devnull rm -Rf "$@"; }
+cp_rf() { if [ -e "$1" ]; then devnull cp -Rfa "$@"; fi; }
+mv_f() { if [ -e "$1" ]; then devnull mv -f "$@"; fi; }
+ln_rm() { devnull find "$HOME" -xtype l -delete; }
+ln_sf() {
+  devnull ln -sf "$@"
+  ln_rm
+}
