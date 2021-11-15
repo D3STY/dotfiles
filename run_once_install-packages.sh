c74778933ee15a0f18ed97d06107591d8bbc2787
D3STY <nitram88@gmx.net>
2021-11-15 06:47:34 +0100

    Add dot_ssh/authorized_keys.tmpl Add run_once_install-packages.sh

---
+++ run_once_install-packages.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+sudo apt install python3 python3-pip python3-setuptools tmux zsh zsh-antigen virtualenv mc htop screen build-essential aria2 fzy net-tools autojump nano cmake
+python3 -m pip install --upgrade certbot pip setuptools wheel youtube-dl yt-dlp tidal-dl mutagen
